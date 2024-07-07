f80796f0c3525b3b55f0d2547757408b1a79f8a3
Ismoilkhon <ismoilkhon.bakhromov@mail.ru>
2024-07-07 21:13:14 +0500

    refactor: project files refactored and some changes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 /* eslint-disable react/react-in-jsx-scope */
 import "./App.css";
-import { ErrorBoundary } from "./components/errorBoundary";
 import Products from "./pages/products";
 
 function App() {
-  return (
-    <ErrorBoundary>
-      <Products />
-    </ErrorBoundary>
-  );
+  return <Products />;
 }
 
 export default App;
